f71bd8117e463d7fb751768d0623b31ea3087024
llullanqui <luis.1142@hotmail.com>
2019-06-26 09:01:32 -0500

    Se añadieron bushes

---
+++ Assets/MovimientoCaja.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCaja : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mover();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Mover()
+    {
+        rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+    }
+}
