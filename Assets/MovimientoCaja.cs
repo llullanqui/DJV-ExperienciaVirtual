539bd5af5e79eb42b6aaa485d5fde4d58778214a
llullanqui <luis.1142@hotmail.com>
2019-06-26 19:58:25 -0500

    ..

--- Assets/MovimientoCaja.cs
+++ Assets/MovimientoCaja.cs
@@ -6,25 +6,35 @@ public class MovimientoCaja : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public float torqueSpeed;
+    public float movementSpeed;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Mover();
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            torqueSpeed = -15;
+            movementSpeed = (float)0.3;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+            torqueSpeed = 15;
+            movementSpeed = (float)-0.3;
+        } else {
+            torqueSpeed = 0;
+            movementSpeed = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            rb.AddForce(new Vector2(0,10), ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate()
     {
-        
+        rb.AddTorque (torqueSpeed, ForceMode2D.Force);
+        rb.AddForce (new Vector2(movementSpeed,0),ForceMode2D.Impulse);
     }
 
-    void Mover()
-    {
-        rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
-    }
 }
